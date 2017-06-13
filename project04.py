aceff7a776f95059fe7581e0be68766d196e5b69
i-Michael Curry <mcurry@numerix.com>
2017-06-13 17:57:39 -0400

    Add test cases

--- project04.py
+++ project04.py
@@ -2,9 +2,11 @@
 # SSW 555 Project 03
 # Written in Python 3
 
-import sys
+import sys, unittest
 from datetime import datetime
+
 from prettytable import PrettyTable
+from inspect import signature
 
 TAGS = {
     "INDI": "0", "NAME": "1", "SEX": "1",
@@ -37,8 +39,7 @@ def persist(buffer):
         # create a tuple that is used to check for uniqueness
         name_birthday = (current.get("NAME", ""), current.get("BIRT"))
         if name_birthday in NAME_AND_BIRTHDAY:
-            print("Error: Individual", name_birthday, "already exists!")
-            sys.exit(1)
+            raise ValueError("Duplicate individual detected in file:", name_birthday)
         else:
             # add user into the collection
             INDI[current.get("INDI")] = current
@@ -46,6 +47,41 @@ def persist(buffer):
             NAME_AND_BIRTHDAY.append(name_birthday)
     elif current.get("FAM"):
         FAM[current.get("FAM")] = current
+
+# helper functions for table
+def lookup_name(ident):
+    # looks up name for given ID
+    if ident:
+        return INDI.get(ident, {}).get("NAME")
+    return None
+
+def calc_age(dob):
+    # age calculator
+    try:
+        today = datetime.today()
+        birthday = datetime.strptime(dob, "%d %b %Y")
+        return today.year - birthday.year - (1 if (today.month, today.day) <
+                                            (birthday.month, birthday.day) else 0)
+    except:
+        return 0
+
+def get_children(ident):
+    children = []
+    marriages = INDI.get(ident, {}).get("FAMS", [])
+    for marriage in marriages:
+        children += FAM.get(marriage, {}).get("CHIL", [])
+    return children or "NA"
+
+
+def get_spouse(ident):
+    spouses = []
+    marriages = INDI.get(ident, {}).get("FAMS", [])
+    for marriage in marriages:
+        husb = FAM.get(marriage, {}).get("HUSB")
+        wife = FAM.get(marriage, {}).get("WIFE")
+        spouses.append(husb if husb != ident else wife)
+    return spouses
+
 
 try:
     f = open("example.ged")
@@ -101,66 +137,71 @@ for line in f:
             else:
                 current[tag] = arguments
 
+f.close()
 
 # persist final buffer
 if current:
     persist(current)
 
-# helper functions for table
-def lookup_name(ident):
-    # looks up name for given ID
-    if ident:
-        return INDI.get(ident, {}).get("NAME")
-    return None
-
-def calc_age(dob):
-    # age calculator
-    try:
-        today = datetime.today()
-        birthday = datetime.strptime(dob, "%d %b %Y")
-        return today.year - birthday.year - (1 if (today.month, today.day) <
-                                            (birthday.month, birthday.day) else 0)
-    except:
-        return 0
-
-def get_children(ident):
-    children = []
-    marriages = INDI.get(ident, {}).get("FAMS", [])
-    for marriage in marriages:
-        children += FAM.get(marriage, {}).get("CHIL", [])
-    return children or "NA"
 
 
-def get_spouse(ident):
-    spouses = []
-    marriages = INDI.get(ident, {}).get("FAMS", [])
-    for marriage in marriages:
-        husb = FAM.get(marriage, {}).get("HUSB")
-        wife = FAM.get(marriage, {}).get("WIFE")
-        spouses.append(husb if husb != ident else wife)
-    return spouses
 
 
-# setup the identity table
-id_table = PrettyTable()
-id_table.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive",
-                        "Death", "Child", "Spouse"]
-for key, person in INDI.items():
-    id_table.add_row([key, person.get("NAME"), person.get("SEX"),
-     person.get("BIRT"), calc_age(person.get("BIRT")), (person.get("DEAT") == None), person.get("DEAT", "NA"),
-     get_children(key), get_spouse(key)])
+if __name__ == "__main__":
+    # setup the identity table
+    id_table = PrettyTable()
+    id_table.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive",
+                            "Death", "Child", "Spouse"]
+    for key, person in INDI.items():
+        id_table.add_row([key, person.get("NAME"), person.get("SEX"),
+                          person.get("BIRT"), calc_age(person.get("BIRT")), (person.get("DEAT") == None),
+                          person.get("DEAT", "NA"),
+                          get_children(key), get_spouse(key)])
 
-print("Individuals")
-print(id_table)
+    print("Individuals")
+    print(id_table)
+
+    fam_table = PrettyTable()
+    fam_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name",
+                             "Wife ID", "Wife Name", "Children"]
+    for key, family in FAM.items():
+        fam_table.add_row([family.get("FAM"), family.get("MARR", "NA"), family.get("DIV", "NA"),
+                           family.get("HUSB"), lookup_name(family.get("HUSB")), family.get("WIFE"),
+                           lookup_name(family.get("WIFE")), str(family.get("CHIL"))])
+
+    print("Families")
+    print(fam_table)
 
 
-fam_table = PrettyTable()
-fam_table.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name",
-                        "Wife ID", "Wife Name", "Children"]
-for key, family in FAM.items():
-    fam_table.add_row([family.get("FAM"), family.get("MARR", "NA"), family.get("DIV", "NA"),
-    family.get("HUSB"), lookup_name(family.get("HUSB")), family.get("WIFE"),
-    lookup_name(family.get("WIFE")), str(family.get("CHIL"))])
+class TestUS23(unittest.TestCase):
+    def test1(self):
+        # ensure the persist() function exists in the program
+        self.failUnless(persist is not None)
+    def test2(self):
+        self.assertEqual(len(signature(persist).parameters), 1, msg="Incorrect number of params for persist()")
+    def test3(self):
+        # remove everything
+        INDI.clear()
+        example = {'INDI': '@I1@', 'NAME': 'Hayley /Dunfee/', 'SEX': 'F', 'BIRT': '10 DEC 1993', 'FAMC': '@F1@'}
+        persist(example)
+        # make sure user can be added in normally
+        expected = {'@I1@': example}
+        self.assertEqual(INDI, expected, msg="Persist did not function as expected")
+    def test4(self):
+        # try to insert duplicate:
+        try:
+            example = {'INDI': '@I1@', 'NAME': 'Hayley /Dunfee/', 'SEX': 'F', 'BIRT': '10 DEC 1993', 'FAMC': '@F1@'}
+            persist(example)
+            self.fail("Duplicate error was not raised")
+        except:
+            pass
+    def test5(self):
+        # ensure return value is set to None
+        example = {'INDI': '@I1@', 'NAME': 'Hayley /Dunfee/', 'SEX': 'F', 'BIRT': '10 DEC 1993', 'FAMC': '@F1@'}
+        res = persist(example)
+        self.assertEqual(res, None, msg="Incorrect return type")
 
-print("Families")
-print(fam_table)
+
+# run automated tests using unittest
+def run_test_harness():
+    unittest.main()
